61798ffdfae765205a6e389c211a9e852f49c57f
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-07-08 00:01:50 -0300

    ➕build: inicio projeto backend
    Project start with SpringBoot

---
+++ lista-de-contatos-backend/src/main/java/projeto/henrique/listadecontatosbackend/ListaDeContatosBackendApplication.java
@@ -0,0 +1,13 @@
+package projeto.henrique.listadecontatosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeContatosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeContatosBackendApplication.class, args);
+	}
+
+}
---
+++ lista-de-contatos-backend/src/test/java/projeto/henrique/listadecontatosbackend/ListaDeContatosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.henrique.listadecontatosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeContatosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
